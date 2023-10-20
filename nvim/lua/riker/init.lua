9786733517a64526d6cc32301d193d6fbb18f561
Kelvin Riker <kelvin.nielsen@spirii.dk>
2023-10-20 07:24:24 +0200

    First init

---
+++ nvim/lua/riker/init.lua
@@ -0,0 +1,6 @@
+require("riker.remap")
+require("riker.set")
+
+
+
+ 
---
+++ nvim/lua/riker/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
